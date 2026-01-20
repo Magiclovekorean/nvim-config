cd5802dca0631209b54d60bd8d5a22ff29502293
Martí Forn <magiclovett@gmail.com>
2026-01-20 22:28:56 +0100

    Update lazy-lock.json with new plugin dependencies and enhance Treesitter configuration with autocommands for various file types.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,27 @@
-print("j")
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+
+  dependencies = {
+    'windwp/nvim-ts-autotag',
+  },
+
+  config = function()
+    
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = {
+        'html',
+        'css',
+        'javascript',
+        'javascriptreact',
+        'typescriptreact',
+        'python',
+        'markdown',
+      },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
