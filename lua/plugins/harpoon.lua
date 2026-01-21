a193b7cd31a08b174a106dce7903bd92508fdb6a
Martí Forn <magiclovett@gmail.com>
2026-01-21 14:52:41 +0100

    Enhance Harpoon plugin configuration by adding key mappings for removing files and improving descriptions for existing mappings. Update Telescope integration for better usability when grepping and listing Harpoon files.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,59 +7,44 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function()
-            harpoon:list():add()
-        end)
+        -- Basic keymaps
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle menu" })
+        vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end, { desc = "Harpoon: Previous file" })
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end, { desc = "Harpoon: Next file" })
+        
+        -- NEW: Remove current file from Harpoon list
+        vim.keymap.set("n", "<C-h>", function()
+            harpoon:list():remove()
+        end, { desc = "Harpoon: Remove current file" })
 
-        vim.keymap.set("n", "<C-e>", function()
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-        end)
-
-        vim.keymap.set("n", "<C-p>", function()
-            harpoon:list():prev()
-        end)
-
-        vim.keymap.set("n", "<C-n>", function()
-            harpoon:list():next()
-        end)
-
+        -- Grep inside Harpoon files (Telescope)
         vim.keymap.set("n", "<leader>hg", function()
-            local harpoon = require("harpoon")
             local conf = require("telescope.config").values
             local themes = require("telescope.themes")
-
-            -- Collect file paths from Harpoon list
             local file_paths = {}
+            
             for _, item in ipairs(harpoon:list().items) do
                 table.insert(file_paths, item.value)
             end
 
-            -- Telescope live_grep restricted to Harpoon files
             require("telescope.pickers").new(
-                themes.get_ivy({
-                    prompt_title = "Grep Harpoon Files",
-                }),
+                themes.get_ivy({ prompt_title = "Grep Harpoon Files" }),
                 {
                     finder = require("telescope.finders").new_oneshot_job(
-                        vim.tbl_flatten({
-                            "rg",
-                            "--color=never",
-                            "--no-heading",
-                            "--with-filename",
-                            "--line-number",
-                            "--column",
-                            "",
-                            "--",
-                            unpack(file_paths),
-                        }),
-                        {}
+                       vim.tbl_flatten({
+                            "rg", "--color=never", "--no-heading",
+                            "--with-filename", "--line-number", "--column",
+                            "", "--", unpack(file_paths)
+                        }), {}
                     ),
                     previewer = conf.grep_previewer({}),
                     sorter = conf.generic_sorter({}),
                 }
             ):find()
-        end, { desc = "Grep inside Harpoon files" })
+        end, { desc = "Harpoon: Grep files" })
 
+        -- Harpoon files list (Telescope)
         vim.keymap.set("n", "<leader>ht", function()
             local conf = require("telescope.config").values
             local themes = require("telescope.themes")
@@ -70,18 +55,14 @@ return {
             end
 
             require("telescope.pickers").new(
-                themes.get_ivy({
-                    prompt_title = "Working List",
-                }),
+                themes.get_ivy({ prompt_title = "Harpoon Files" }),
                 {
-                    finder = require("telescope.finders").new_table({
-                        results = file_paths,
-                    }),
+                    finder = require("telescope.finders").new_table({ results = file_paths }),
                     previewer = conf.file_previewer({}),
                     sorter = conf.generic_sorter({}),
                 }
             ):find()
-        end, { desc = "Open harpoon window" })
+        end, { desc = "Harpoon: List files" })
     end,
 }
 
